e2ea29080d815fffad5ad88d0ae2f30e71e0275b
ba-on <messr.rey@gmail.com>
2020-04-17 02:54:50 -0700

    Implement resize and RGB conversion, file format

--- changeImage.py
+++ changeImage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# TODO: Test tomorrow with fresh lab, too tired to continue past 3:00AM
+
 import os
 from PIL import Image 
 
@@ -7,4 +9,7 @@ IMG_SOURCE = '/home/student-04-8997365d472f/supplier-data/images/'
 
 for image in os.listdir(IMG_SOURCE):
     if image.endswith('.tiff'):
-        
+        file, ext = os.splitext(image)
+        new_im = Image.open(image)
+        new_im.convert("RGB").resize((600, 400)).save(IMG_SOURCE + file + '.jpeg', "JPEG")
+
